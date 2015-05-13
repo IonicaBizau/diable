33bc3d3c8d9a0224e597f93b3451d02a8468a058
Ionică Bizău <bizauionica@yahoo.com>
2015-05-13 18:28:33 +0300

    Added a new example

---
+++ example/bg-server.js
@@ -0,0 +1,14 @@
+// Dependencies
+var Diable = require("../lib")
+  , Http = require("http")
+  , Fs = require("fs")
+  ;
+
+// Create the daemon
+Diable();
+
+// This is run only when the process is a daemon
+Http.createServer(function (req, res) {
+    Fs.writeFileSync("foo", req.url);
+    res.end("Hello World");
+}).listen(9000);
