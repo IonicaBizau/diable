c4667d65abf7e2c246b5007754474099cc25711e
Ionică Bizău <bizauionica@yahoo.com>
2015-05-13 14:20:51 +0300

    No callback function anymore.

--- example/index.js
+++ example/index.js
@@ -3,6 +3,4 @@ var Diable = require("../lib");
 
 // Do the stuff
 console.log("Daemonizing this process.");
-Diable(function (err) {
-    console.log(err || "Do `ps aux | grep node` and see the daemons.");
-});
+Diable();
