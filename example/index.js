0d3871c437c139968d49643894e8c90292f5ec86
Ionică Bizău <bizauionica@yahoo.com>
2015-05-13 14:32:39 +0300

    Added the isDaemon method. Kill the parent process

--- example/index.js
+++ example/index.js
@@ -1,6 +1,13 @@
 // Dependencies
 var Diable = require("../lib");
 
+// Check if we are already a daemon
+if (Diable.isDaemon()) {
+    return setTimeout(function () {
+        console.log("I was alive for 10 seconds.");
+    }, 10000);
+}
+
 // Do the stuff
 console.log("Daemonizing this process.");
 Diable();
