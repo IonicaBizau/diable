7fc1ce01b4110327f92e75308b4d0371840830ba
Ionică Bizău <bizauionica@gmail.com>
2015-09-13 11:23:14 +0300

    Updated the example

--- example/index.js
+++ example/index.js
@@ -3,11 +3,14 @@ var Diable = require("../lib");
 
 // Check if we are already a daemon
 if (Diable.isDaemon()) {
-    return setTimeout(function () {
+    setTimeout(function () {
         console.log("I was alive for 10 seconds.");
     }, 10000);
+} else {
+    console.log("Daemonizing this process. I will be killed, but I have a child which will live 10 seconds. Do `ps aux | grep node` to see it.");
 }
 
-// Do the stuff
-console.log("Daemonizing this process. I will be killed, but I have a child which will live 10 seconds. Do `ps aux | grep node` to see it.");
+// Daemonize this process (only the non-daemons)
 Diable();
+
+console.log("I am a daemon.");
