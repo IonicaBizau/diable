c9b8711bd7c8bedfc85dc4ba6ae051cfbcda4f98
Ionică Bizău <bizauionica@gmail.com>
2017-12-24 21:16:02 +0200

    :arrow_up: 3.0.2 :tada:

--- example/bg-server.js
+++ example/bg-server.js
@@ -1,14 +1,15 @@
+"use strict";
+
 // Dependencies
-var Diable = require("../lib")
-  , Http = require("http")
-  , Fs = require("fs")
-  ;
+var Diable = require("../lib"),
+    Http = require("http"),
+    Fs = require("fs");
 
 // Create the daemon
 Diable();
 
 // This is run only when the process is a daemon
 Http.createServer(function (req, res) {
-    Fs.writeFileSync("foo", req.url);
-    res.end("Hello World " + process.argv[2]);
+  Fs.writeFileSync("foo", req.url);
+  res.end("Hello World " + process.argv[2]);
 }).listen(9000);
--- example/index.js
+++ example/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Dependencies
 var Diable = require("../lib");
 
