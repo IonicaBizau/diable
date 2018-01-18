47d68f813cd461432ed662cf1f69d56b2d21fcb9
Ionică Bizău <bizauionica@gmail.com>
2018-01-18 12:56:04 +0200

    Fix tests

--- test/index.js
+++ test/index.js
@@ -1,18 +1,16 @@
-// Dependencies
-var Diable = require("../lib")
-  , Assert = require("assert")
-  ;
+const Diable = require("../lib")
+    , Assert = require("assert")
 
 // Check if it's a daemon
 if (Diable.isDaemon()) {
-    return setTimeout(function () {
+    return setTimeout(() => {
         console.log("Was alive for 1000ms");
     }, 1000);
+} else {
+    // Daemonize this process
+    it("shuould daemonize the process itself", cb => {
+        const daemon = Diable({ exit: false })
+        Assert.equal(typeof daemon.pid, "number")
+        cb()
+    })
 }
-
-// Daemonize this process
-it("shuould daemonize the process itself", function (cb) {
-    var daemon = Diable({ exit: false });
-    Assert.equal(typeof daemon.pid, "number");
-    cb();
-});
